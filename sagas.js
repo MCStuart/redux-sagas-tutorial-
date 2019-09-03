aae068dbb6e9645a3bf784985ef09312d5e68c5f
Stuart McKay <stuart51994@gmail.com>
2019-09-02 21:39:41 -0700

    add tests and change sagas entry point to rootSaga

--- sagas.js
+++ sagas.js
@@ -1,18 +1,28 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
-export function* helloSaga() {
+function* helloSaga() {
     console.log('Hello Sagas!')
 }
 
 // Our worker Saga: will perform the async increment task
 export function* incrementAsync() {
+    // use the call Effect
     yield delay(1000) 
     yield put({ type: "INCREMENT" })
 }
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
-export function* watchIncrementAsync() {
+function* watchIncrementAsync() {
     yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// Now only export the rootSaga
+// single entry point to start all Sagas at once
+export default function* rootSaga() {
+    yield all([
+        helloSaga(),
+        watchIncrementAsync()
+    ])
 }
